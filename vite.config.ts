ceb3b7c8448fb381739fb1e8624ad89081987015
ASmolyaninov <a.smolyaninov@brightlab.me>
2022-05-19 15:06:16 +0300

    little fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import TsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist',
+  base: './',
   plugins: [react(), TsconfigPaths()]
 })
